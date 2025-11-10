883117de2fdf453551af6e9cec91b39b7f715177
Day Munoz <daymrz@Days-MacBook-Air.local>
2025-11-09 21:37:41 -0500

    theme and layout test

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,49 +1,53 @@
 
 import { Link, Outlet } from 'react-router-dom'
-import { AppBar, CssBaseline, Toolbar, Typography, Box, Button, Container } from "@mui/material"
+import { AppBar, CssBaseline, Toolbar, Typography, Box, Button, Container, ThemeProvider } from "@mui/material"
 import {Home as HomeIcon} from '@mui/icons-material'
+
+import theme from '../theme/theme'
 
 const Layout = () => {
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#f4f7f6' }}>
-      <CssBaseline/>
-      <AppBar position='sticky' sx={{ bgColor: '#4a148c' }}>
-        <Toolbar>
-          <Typography
-            variant='h6'
-            component={Link}
-            to='/'
-            onClick={'/'}
-            sx={{ flexGrow: 1, fontWeight: 700, textDecoration: 'none', color: 'white' }}
-          >
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', backgroundColor: '#f4f7f6' }}>
+        <CssBaseline/>
+        <AppBar position='sticky'>
+          <Toolbar>
+            <Typography
+              variant='h6'
+              component={Link}
+              to='/'
+              onClick={'/'}
+              sx={{ flexGrow: 1, fontWeight: 700, textDecoration: 'none', color: 'white' }}
+            >
 
-          </Typography>
+            </Typography>
 
-          <Button color='inherit' component={Link} to='/' onClick={'/'} startIcon={<HomeIcon />} >
-            Home
-          </Button>
-        </Toolbar>
-      </AppBar>
+            <Button color='inherit' component={Link} to='/' onClick={'/'} startIcon={<HomeIcon />} >
+              Home
+            </Button>
+          </Toolbar>
+        </AppBar>
 
-      <Container
-        maxWidth='lg'
-        sx={{
-          flexGrow: 1,
-          pt: 4,
-          pb: 8,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'flex-start'
-        }}
-      >
-        <Outlet />
-      </Container>
+        <Container
+          maxWidth='lg'
+          sx={{
+            flexGrow: 1,
+            pt: 4,
+            pb: 8,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'flex-start'
+          }}
+        >
+          <Outlet />
+        </Container>
 
-      <Box component='footer' sx={{ py: 3, bgcolor: 'grey.200', textAlign: 'center' }}>
+        <Box component='footer' sx={{ py: 3, bgcolor: 'grey.200', textAlign: 'center' }}>
 
+        </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   )
 }
 
