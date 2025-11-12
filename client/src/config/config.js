e7b8985c137177ed13e1745886d42df84f81da28
Day Munoz <daymrz@dyn194013.cc.lehigh.edu>
2025-11-12 18:59:31 -0500

    route examples

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -4,21 +4,24 @@ import { Home as HomeIcon,
   Star,
 } from '@mui/icons-material'
 
+
+import HomePage from   '../pages/HomePage'
+import SearchPage from '../pages/SearchPage'
+import MyListPage from '../pages/MyListPage'
+import ProfilePage from '../pages/ProfilePage'
+
 export const PUBLIC_NAVIGATION_LINKS = [
   {
     name: 'Anime',
     path: '/',
-    icon: HomeIcon
+    icon: HomeIcon,
+    component: HomePage,
   },
   {
-        name: 'Search',
-        path: '/search',
-        icon: SearchIcon,
-    },
-  {
-    name: 'My List',
-    path: '/list',
-    icon: Star,
+    name: 'Search',
+    path: '/search',
+    icon: SearchIcon,
+    component: SearchPage,
   },
 ]
 
@@ -27,14 +30,19 @@ export const AUTH_NAVIGATION_LINKS = [
     name: 'Profile',
     path: '/profile',
     icon: ProfileIcon,
+    component: ProfilePage,
   },
   {
     name: 'My List',
     path: '/list',
     icon: Star,
+    component: MyListPage
+
   }
 ]
 
 export const API_BASE_URL = 'https://api.anime.com/v1'
+
+export const APP_NAME = 'AnimePulse'
 
 //here we have to define our anime app name, because we dont have it yet, we will use a placeholder
