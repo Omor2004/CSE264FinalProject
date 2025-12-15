e7dd8a8bd79b96c1f7bc5e7ff16cffeb280ff3b6
Day Munoz <daymrz@Days-MacBook-Air.local>
2025-12-14 21:51:40 -0500

    incoming repetitive

--- server/app.js
+++ server/app.js
@@ -16,22 +16,6 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Allow cookies/authorization headers
-  optionsSuccessStatus: 204
-}
-app.use(cors(corsOptions))
-
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // Allow cookies/authorization headers
-  optionsSuccessStatus: 204
-}
-app.use(cors(corsOptions))
-
 app.use(express.json())
 // app.use(cors()) 
 
